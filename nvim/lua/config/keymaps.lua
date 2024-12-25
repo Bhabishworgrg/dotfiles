110ef4c7ae306723ef2c4590e9ef984397c356be
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-12-25 22:32:38 +0545

    Add new keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,40 +1,34 @@
--- Nvim
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)		-- open file explorer
-
-
--- For Plugins
+local set = vim.keymap.set
+local cmd = vim.cmd
 local fn = require('config.lazy')
 
+-- Nvim
+set('n', '<leader>e', cmd.Ex)	-- open file explorer
 
 -- Telescope
--- search files
-vim.keymap.set('n', '<leader><leader>', function()
-	fn.telescope('find_files')
-end)
-
--- search inside of files
-vim.keymap.set('n', '<leader>f', function()
-	fn.telescope('live_grep')
-end)
-
+set('n', '<leader><leader>', function() fn.telescope('find_files') end)	-- search files
+set('n', '<leader>f', function() fn.telescope('live_grep') end)			-- search inside of files
 
 -- Fugitive
-vim.keymap.set('n', '<leader>g', vim.cmd.Git)	-- show git status
-
+set('n', '<leader>g', cmd.Git)	-- show git status
 
 -- Harpoon
-local harpoon = require('harpoon')
-harpoon:setup()
+local list = require('harpoon'):list()
+set('n', '<leader>h', function() fn.harpoon(list) end, { desc = 'Open harpoon window' })	-- open harpoon window
+set('n', '<leader>a', function() list:add() end)		-- add file
+set('n', '<leader>r', function() list:remove() end)		-- remove all files
+set('n', '<leader>1', function() list:select(1) end)	-- open 1st file
+set('n', '<leader>2', function() list:select(2) end)	-- open 2nd file
+set('n', '<leader>3', function() list:select(3) end)	-- open 3rd file
+set('n', '<leader>4', function() list:select(4) end)	-- open 4th file
+set('n', '<leader>5', function() list:select(5) end)	-- open 5th file
 
--- open harpoon window
-vim.keymap.set('n', '<leader>h', function()
-	fn.harpoon(harpoon:list())
-end, { desc = 'Open harpoon window' })
+-- Flog
+set('n', '<leader>l', cmd.Flogsplit)	-- open flog window
 
-vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)		-- add file
-vim.keymap.set('n', '<leader>r', function() harpoon:list():remove() end)	-- remove all files
-vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)	-- open 1st file
-vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)	-- open 2nd file
-vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)	-- open 3rd file
-vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)	-- open 4th file
-vim.keymap.set('n', '<leader>5', function() harpoon:list():select(5) end)	-- open 5th file
+-- Undotree
+set('n', '<leader>u', function () cmd.UndotreeToggle() cmd.UndotreeFocus() end)	-- open undotree window
+
+-- Gutter
+set('n', '<leader>j', cmd.GitGutterNextHunk)	-- next hunk
+set('n', '<leader>k', cmd.GitGutterPrevHunk)	-- previous hunk
