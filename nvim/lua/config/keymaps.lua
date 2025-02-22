dd8661415b2a3a88f5563fef2b3d03e9b32296aa
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-22 14:08:19 +0545

    Add more harpoon buffers

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -22,6 +22,11 @@ set('n', '<leader>2', function() list:select(2) end)	-- open 2nd file
 set('n', '<leader>3', function() list:select(3) end)	-- open 3rd file
 set('n', '<leader>4', function() list:select(4) end)	-- open 4th file
 set('n', '<leader>5', function() list:select(5) end)	-- open 5th file
+set('n', '<leader>6', function() list:select(6) end)	-- open 6th file
+set('n', '<leader>7', function() list:select(7) end)	-- open 7th file
+set('n', '<leader>8', function() list:select(8) end)	-- open 8th file
+set('n', '<leader>9', function() list:select(9) end)	-- open 9th file
+set('n', '<leader>0', function() list:select(10) end)	-- open 10th file
 
 -- Flog
 set('n', '<leader>l', cmd.Flogsplit)	-- open flog window
