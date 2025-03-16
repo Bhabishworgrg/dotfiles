67422428b3d882f849dd01e22a942a54f5290af1
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-16 18:25:11 +0545

    Fix volume going over 100 bug

--- i3/volume_manager.sh
+++ i3/volume_manager.sh
@@ -4,7 +4,9 @@ volume="$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%' | head -n1)"
 
 case $1 in
     up)
-		if [ "${volume%?}" -ne 100 ]; then
+		if [ "${volume%?}" -eq 99 ]; then
+			pactl set-sink-volume @DEFAULT_SINK@ 100%
+		elif [ "${volume%?}" -ne 100 ]; then
 			pactl set-sink-volume @DEFAULT_SINK@ +2%
 		fi
 		notify-send "Volume" -h int:value:${volume%?} -h string:synchronous:download -u low -i /usr/share/icons/Adwaita/symbolic/status/audio-volume-high-symbolic.svg
