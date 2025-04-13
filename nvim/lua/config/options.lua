f69a5f7a36eef23e80e0273c54529b846236f9ce
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-13 20:42:09 +0545

    Update netrw config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -15,3 +15,8 @@ vim.cmd[[colorscheme tokyonight]]
 -- Transparent background
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 
+-- Netrw
+vim.g.netrw_banner = false
+vim.g.netrw_browse_split = 3
+vim.g.netrw_winsize = 75
+vim.g.netrw_liststyle = 3
