3a9af99428e1ea05bfa242183e98c8951ca770a7
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-09-27 22:03:47 +0545

    Only start java server if dependency management files detected

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -58,28 +58,7 @@ lsp('gdscript', {
 	name = 'godot',
 	cmd = vim.lsp.rpc.connect('127.0.0.1', 6005),
 })
-
--- Fix matlab's root directory
-lsp('matlab_ls', {
-	cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/matlab-language-server'), '--stdio' },
-	root_dir = function(fname)
-		return require('lspconfig.util').root_pattern('.git')(fname) or vim.fn.getcwd()
-	end,
-})
-
--- Fix kotlin's root directory
-lsp('kotlin_language_server', {
-	cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/kotlin-language-server') },
-	root_dir = function(fname)
-		return require('lspconfig.util').root_pattern('.git')(fname) or vim.fn.getcwd()
-	end,
-	init_options = {
-		-- Set a proper storage path as a string.
-		storagePath = vim.fn.expand('~/.cache/kotlin-language-server'),
-	},
-})
-
-vim.lsp.enable({ 'gdscript', 'matlab_ls', 'kotlin_language_server' })
+vim.lsp.enable({ 'gdscript' })
 
 -- Setup telescope layout and lsp code actions
 local telescope = require('telescope')
@@ -101,75 +80,78 @@ telescope.setup({
 telescope.load_extension('ui-select')
 
 -- Setup Java
---local home = os.getenv 'HOME'
---local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
---local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
---local workspace_dir = workspace_path .. project_name
---
---local status, jdtls = pcall(require, 'jdtls')
---if not status then
---	return
---end
---local extendedClientCapabilities = jdtls.extendedClientCapabilities
---
---local config = {
---	cmd = {
---		'java',
---		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
---		'-Dosgi.bundles.defaultStartLevel=4',
---		'-Declipse.product=org.eclipse.jdt.ls.core.product',
---		'-Dlog.protocol=true',
---		'-Dlog.level=ALL',
---		'-Xmx1g',
---		'--add-modules=ALL-SYSTEM',
---		'--add-opens',
---		'java.base/java.util=ALL-UNNAMED',
---		'--add-opens',
---		'java.base/java.lang=ALL-UNNAMED',
---		'-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
---		'-jar',
---		vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
---		'-configuration',
---		home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
---		'-data',
---		workspace_dir,
---	},
---	root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
---
---	settings = {
---		java = {
---			signatureHelp = { enabled = true },
---			extendedClientCapabilities = extendedClientCapabilities,
---			maven = {
---				downloadSources = true,
---			},
---			referencesCodeLens = {
---				enabled = true,
---			},
---			references = {
---				includeDecompiledSources = true,
---			},
---			inlayHints = {
---				parameterNames = {
---					enabled = 'all',
---				},
---			},
---			format = {
---				enabled = false,
---			},
---		},
---	},
---
---	init_options = {
---		bundles = {},
---	},
---}
---require('jdtls').start_or_attach(config)
+local root_dir = require('jdtls.setup').find_root { 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
 
---local jdtls_config = {
---  bundles = {}
---}
---vim.list_extend(jdtls_config.bundles, require('spring_boot').java_extensions())
+if root_dir then
+	local workspace_path = vim.fn.stdpath('data') .. '/jdtls-workspace/'
+	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+	local workspace_dir = workspace_path .. project_name
+
+	local status, jdtls = pcall(require, 'jdtls')
+	if not status then
+		return
+	end
+	local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+	local config = {
+		cmd = {
+			'java',
+			'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+			'-Dosgi.bundles.defaultStartLevel=4',
+			'-Declipse.product=org.eclipse.jdt.ls.core.product',
+			'-Dlog.protocol=true',
+			'-Dlog.level=ALL',
+			'-Xmx1g',
+			'--add-modules=ALL-SYSTEM',
+			'--add-opens',
+			'java.base/java.util=ALL-UNNAMED',
+			'--add-opens',
+			'java.base/java.lang=ALL-UNNAMED',
+			'-javaagent:' .. vim.fn.stdpath('data') .. '/mason/packages/jdtls/lombok.jar',
+			'-jar',
+			vim.fn.glob(vim.fn.stdpath('data') .. '/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+			'-configuration',
+			vim.fn.stdpath('data') .. '/mason/packages/jdtls/config_linux',
+			'-data',
+			workspace_dir,
+		},
+		root_dir = root_dir,
+
+		settings = {
+			java = {
+				signatureHelp = { enabled = true },
+				extendedClientCapabilities = extendedClientCapabilities,
+				maven = {
+					downloadSources = true,
+				},
+				referencesCodeLens = {
+					enabled = true,
+				},
+				references = {
+					includeDecompiledSources = true,
+				},
+				inlayHints = {
+					parameterNames = {
+						enabled = 'all',
+					},
+				},
+				format = {
+					enabled = false,
+				},
+			},
+		},
+
+		init_options = {
+			bundles = {},
+		},
+	}
+	require('jdtls').start_or_attach(config)
+
+	local jdtls_config = {
+		bundles = {}
+	}
+	vim.list_extend(jdtls_config.bundles, require('spring_boot').java_extensions())
+end
 
 -- Setup required functions
 local fn = {}
