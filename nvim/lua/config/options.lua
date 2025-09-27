c78034fc62245fdf3465c10c00d7aab3e7cac4b3
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-09-27 19:08:45 +0545

    Call global color setting attribute only once

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -19,17 +19,15 @@ vim.opt.termguicolors = true
 vim.api.nvim_create_autocmd('ColorScheme', {
 	pattern = 'tokyonight',
 	callback = function()
-		vim.api.nvim_set_hl(0, '@variable', { fg = '#f7768e' }) -- override variable color
+		local set_hl = vim.api.nvim_set_hl
+		set_hl(0, '@variable', { fg = '#f7768e' }) 		-- variable color
+		set_hl(0, 'Normal', { bg = 'none' })			-- main window
+		set_hl(0, 'NormalFloat', { bg = '#000000' })	-- LSP definitions
+		set_hl(0, 'Pmenu', { bg = '#000000' })			-- autocomplete menu
+		set_hl(0, 'EndOfBuffer', { bg = 'none' })		-- non-written lines
 	end,
 })
-
 vim.cmd.colorscheme('tokyonight')
-
--- Transparent background
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })			-- main window
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#000000' })	-- LSP definitions
-vim.api.nvim_set_hl(0, 'Pmenu', { bg = '#000000' })			-- autocomplete menu
-vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })		-- non-written lines
 
 -- Netrw
 vim.g.netrw_banner = false
